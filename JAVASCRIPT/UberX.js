78e273f7284bd239a26783ee88e82bc7acc4927e
Alfredo Martinez <psialfredomtz@gmail.com>
2020-08-13 18:17:05 -0500

    JS herencia!

--- JAVASCRIPT/UberX.js
+++ JAVASCRIPT/UberX.js
@@ -4,4 +4,12 @@ class UberX extends Car {
         this.brand = brand;
         this.model = model;
     }
+
+    printDataUberX () {
+        console.log(this.driver);
+        console.log(this.driver.name);
+        console.log(this.driver.document);
+        console.log(this.brand);
+        console.log(this.model);
+    }
 }
--- JAVASCRIPT/index.js
+++ JAVASCRIPT/index.js
@@ -1,3 +1,7 @@
 var car = new Car ("ADW245", new Account("Alfredo Martinez", "CURP"));
 car.passenger = 4;
 car.printDataCar ();
+
+var uberX = new UberX ("ATR357", new Account("Ixchel B", "IDK978"), "Ford", "Spark")
+uberX.passenger = 4;
+uberX.printDataUberX();
