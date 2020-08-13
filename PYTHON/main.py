15f7f555a2b649a1c195a7b6593453f7cc9bdd91
Alfredo Martinez <psialfredomtz@gmail.com>
2020-08-13 15:28:11 -0500

    Python init

--- PYTHON/main.py
+++ PYTHON/main.py
@@ -1,2 +1,9 @@
+from car import Car
+from account import Account
+
 if __name__ == "__main__":
     print("hola mundo")
+
+    car = Car("Curp23", Account("Alfredo Garcia", "ced2342"))
+    print(vars(car))
+    print(vars(car.driver))
