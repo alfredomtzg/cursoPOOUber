75d60c90d2bdac78e78b8e0a270a11a4c64effa2
Alfredo Martinez <psialfredomtz@gmail.com>
2020-08-13 19:30:18 -0500

    chanllenge 3Â

--- JAVASCRIPT/Payment.js
+++ JAVASCRIPT/Payment.js
@@ -5,7 +5,11 @@
 // EcmaScript6
 
 class Payment {
-    constructor() {
-        this.id;
+    constructor(id) {
+        this.id = id
+    }
+
+    printDataPayment() {
+        console.log(this.id);
     }
 }
