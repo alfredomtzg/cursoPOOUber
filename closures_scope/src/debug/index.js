1e1ee5fb3895d0d7557d55a6d04588e267f6dde4
Alfredo Martinez <psialfredomtz@gmail.com>
2020-08-17 17:13:49 -0500

    Finish course

---
+++ closures_scope/src/debug/index.js
@@ -0,0 +1,13 @@
+var a = "hello";
+
+function hello() {
+    let b = "Hello World";
+    const c = "Hello World!!!";
+    debugger
+    if (true) {
+        let d = "Hello Fucking World";
+        debugger
+    }
+}
+
+hello();
