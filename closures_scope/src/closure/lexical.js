8f114e5d5f532deceef8b001096967a986ce2feb
Alfredo Martinez <psialfredomtz@gmail.com>
2020-08-16 23:34:20 -0500

    Lexico in closures

---
+++ closures_scope/src/closure/lexical.js
@@ -0,0 +1,13 @@
+const buildCount = (i) => {
+    let count = i;
+    const displayCount = () => {
+        console.log(count ++);
+    };
+    return displayCount;
+};
+
+const myCount = buildCount(1);
+
+myCount();
+myCount();
+myCount();
